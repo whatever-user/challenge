b5d2e02f414356dfb28d18100d302a9054f6dba3
Whatever User <eder@gexplorer.org>
2015-07-21 17:11:02 +0200

    Search in tag name instead of ==

--- app/scripts/tiles/tiles-service.js
+++ app/scripts/tiles/tiles-service.js
@@ -16,7 +16,7 @@ angular.module('tiles.service', ['tiles.data', 'tiles.filters'])
                         var inTitle = (tile.title.toUpperCase().indexOf(content.toUpperCase()) >= 0);
                         var inDescription = (tile.description.toUpperCase().indexOf(content.toUpperCase()) >= 0);
                         var foundTags = (tile.tags.filter(function (value) {
-                            return (value.name === content)
+                            return (value.name.indexOf(content) >= 0)
                         }));
                         var inTags = (foundTags.length > 0);
                         return inTitle || inDescription || inTags;
